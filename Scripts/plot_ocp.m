fb2dd3a6664da56d63a2a9404a132dbc5c5cac51
Simon Clark <simon.clark@sintef.no>
2024-11-13 16:21:31 +0100

    Create plot_ocp.m
    a basic script for comparing cell voltage curves from ocp functions

---
+++ Scripts/plot_ocp.m
@@ -0,0 +1,18 @@
+cmax_lnmo = 23286.29529;
+cmax_xno = 35259;
+
+c_lnmo = linspace(cmax_lnmo,0,100);
+c_xno = 0 + (cmax_lnmo-c_lnmo);
+
+ocp_lnmo = computeOCP_LNMO_Morrow(c_lnmo,T,cmax_lnmo);
+ocp_xno = computeOCP_XNO_Morrow(c_xno,T,cmax_xno);
+
+ocp_cell = ocp_lnmo - ocp_xno;
+
+soc = c_lnmo ./ cmax_lnmo;
+
+plot(soc,ocp_lnmo)
+hold on
+plot(soc,ocp_xno)
+hold on
+plot(soc,ocp_cell)
