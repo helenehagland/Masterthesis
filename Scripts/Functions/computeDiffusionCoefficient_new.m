f25d6b4ab9b84b6eca34ffd8424ada082e17528a
helenehagland <helene@hagland.no>
2025-04-04 18:29:53 +0200

    Add script for elyte diffusion

---
+++ Scripts/Functions/computeDiffusionCoefficient_new.m
@@ -0,0 +1,15 @@
+
+function D = computeDiffusionCoefficient_new(c, T)
+    
+    % Calculate diffusion coefficients constant for the diffusion coefficient calculation
+    cnst = [ -4.43, -54; 
+             -0.22, 0.0 ];
+
+    Tgi = [ 229; 5.0 ];
+    
+    % Diffusion coefficient, [m^2 s^-1]
+    D = 100 .* 1e-4 .* 10 .^ ( ( cnst(1,1) + cnst(1,2) ./ ( T - Tgi(1) - Tgi(2) .* c .* 1e-3) + cnst(2,1) .* ...
+                          c .* 1e-3) );
+    
+end
+
